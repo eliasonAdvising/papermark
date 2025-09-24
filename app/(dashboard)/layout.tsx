a00b77b9cd57876357a0c8fcbc871967827a43cc
eliasonAdvising <ieliason@icloud.com>
2025-09-24 09:07:22 -0500

    Update dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,28 +1,26 @@
 'use client';
 
-import { TeamProvider } from "@/context/team-context";
 import { SessionProvider } from "next-auth/react";
-
-import { PostHogCustomProvider } from "@/components/providers/posthog-provider";
+import { Toaster } from "sonner";
+import { TeamProvider } from "@/context/team-context";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function DashboardLayout({
   children,
-}: { 
+}: {
   children: React.ReactNode;
 }) {
   return (
     <SessionProvider>
-      <PostHogCustomProvider>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <Toaster closeButton />
-          <TooltipProvider delayDuration={100}>
-            <TeamProvider>{children}</TeamProvider>
-          </TooltipProvider>
-        </ThemeProvider>
-      </PostHogCustomProvider>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <TooltipProvider delayDuration={100}>
+          <TeamProvider>
+            <Toaster closeButton />
+            {children}
+          </TeamProvider>
+        </TooltipProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
