28acb712ae9081bf5281ea66f3e7661942665a1e
eliasonAdvising <ieliason@icloud.com>
2025-09-26 11:14:46 -0500

    deploy fixes (integration of v4)

--- lib/utils/trigger-utils.ts
+++ lib/utils/trigger-utils.ts
@@ -1,24 +1,7 @@
 import { BasePlan } from "../swr/use-billing";
 
-type TQueueConfig = {
-  name: string;
-  concurrencyLimit: number;
-};
-
-const concurrencyConfig: Record<string, number> = {
-  free: 1,
-  starter: 1,
-  pro: 2,
-  business: 10,
-  datarooms: 10,
-  "datarooms-plus": 10,
-};
-
-export const conversionQueue = (plan: string): TQueueConfig => {
+// In v4, we just return the queue name since queues are pre-defined
+export const conversionQueue = (plan: string): string => {
   const planName = plan.split("+")[0] as BasePlan;
-
-  return {
-    name: `conversion-${planName}`,
-    concurrencyLimit: concurrencyConfig[planName],
-  };
+  return `conversion-${planName}`;
 };
